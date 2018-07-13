373db5cf40bea708aebb16c475447e5b6cb90702
Martin Janiczek <martin@janiczek.cz>
2018-07-13 09:51:44 +0200

    Add progressbar

--- src/Main.elm
+++ src/Main.elm
@@ -27,6 +27,7 @@ main =
 
 type alias Model =
     { exercises : List Exercise
+    , allExercises : Int
     , answerInput : String
     , lastAttempt : Maybe Exercise
     , lastAttemptState : Animation.State
@@ -56,6 +57,7 @@ defaultBgColor =
 init : ( Model, Cmd Msg )
 init =
     ( { exercises = []
+      , allExercises = 0
       , answerInput = ""
       , lastAttempt = Nothing
       , lastAttemptState =
@@ -135,7 +137,10 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ExercisesGenerated exercises ->
-            ( { model | exercises = exercises }
+            ( { model
+                | exercises = exercises
+                , allExercises = List.length exercises
+              }
             , focusCurrentAnswer
             )
 
@@ -234,25 +239,196 @@ update msg model =
 view : Model -> Html Msg
 view model =
     H.div
-        [ HA.style
-            [ ( "display", "grid" )
-            , ( "grid-template-columns", "repeat(3,1fr)" )
-            , ( "font-size", "24px" )
-            ]
-        ]
+        []
         {-
            -- DEBUG
            (model.exercises
                |> List.map (viewExercise model.currentExerciseState model.answerInput)
            )
         -}
-        [ model.exercises
-            |> List.head
-            |> Maybe.map (viewExercise model.currentExerciseState model.answerInput)
-            |> Maybe.withDefault viewWin
-        , model.lastAttempt
-            |> Maybe.map (viewLastAttempt model.lastAttemptState)
-            |> Maybe.withDefault (H.text "")
+        [ H.node "style" [] [ H.text """
+
+/* Taken from https://css-tricks.com/css3-progress-bars/ */
+
+.meter {
+    height: 20px;  /* Can be anything */
+    position: relative;
+    margin: 60px 0 20px 0; /* Just for demo spacing */
+    background: #555;
+    -moz-border-radius: 25px;
+    -webkit-border-radius: 25px;
+    border-radius: 25px;
+    padding: 10px;
+    -webkit-box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3);
+    -moz-box-shadow   : inset 0 -1px 1px rgba(255,255,255,0.3);
+    box-shadow        : inset 0 -1px 1px rgba(255,255,255,0.3);
+}
+.meter > span {
+    display: block;
+    height: 100%;
+       -webkit-border-top-right-radius: 8px;
+    -webkit-border-bottom-right-radius: 8px;
+           -moz-border-radius-topright: 8px;
+        -moz-border-radius-bottomright: 8px;
+               border-top-right-radius: 8px;
+            border-bottom-right-radius: 8px;
+        -webkit-border-top-left-radius: 20px;
+     -webkit-border-bottom-left-radius: 20px;
+            -moz-border-radius-topleft: 20px;
+         -moz-border-radius-bottomleft: 20px;
+                border-top-left-radius: 20px;
+             border-bottom-left-radius: 20px;
+    background-color: rgb(43,194,83);
+    background-image: -webkit-gradient(
+      linear,
+      left bottom,
+      left top,
+      color-stop(0, rgb(43,194,83)),
+      color-stop(1, rgb(84,240,84))
+     );
+    background-image: -moz-linear-gradient(
+      center bottom,
+      rgb(43,194,83) 37%,
+      rgb(84,240,84) 69%
+     );
+    -webkit-box-shadow:
+      inset 0 2px 9px  rgba(255,255,255,0.3),
+      inset 0 -2px 6px rgba(0,0,0,0.4);
+    -moz-box-shadow:
+      inset 0 2px 9px  rgba(255,255,255,0.3),
+      inset 0 -2px 6px rgba(0,0,0,0.4);
+    box-shadow:
+      inset 0 2px 9px  rgba(255,255,255,0.3),
+      inset 0 -2px 6px rgba(0,0,0,0.4);
+    position: relative;
+    overflow: hidden;
+}
+.meter > span:after, .animate > span > span {
+    content: "";
+    position: absolute;
+    top: 0; left: 0; bottom: 0; right: 0;
+    background-image:
+       -webkit-gradient(linear, 0 0, 100% 100%,
+          color-stop(.25, rgba(255, 255, 255, .2)),
+          color-stop(.25, transparent), color-stop(.5, transparent),
+          color-stop(.5, rgba(255, 255, 255, .2)),
+          color-stop(.75, rgba(255, 255, 255, .2)),
+          color-stop(.75, transparent), to(transparent)
+       );
+    background-image:
+        -moz-linear-gradient(
+          -45deg,
+          rgba(255, 255, 255, .2) 25%,
+          transparent 25%,
+          transparent 50%,
+          rgba(255, 255, 255, .2) 50%,
+          rgba(255, 255, 255, .2) 75%,
+          transparent 75%,
+          transparent
+       );
+    z-index: 1;
+    -webkit-background-size: 50px 50px;
+    -moz-background-size: 50px 50px;
+    background-size: 50px 50px;
+    -webkit-animation: move 2s linear infinite;
+    -moz-animation: move 2s linear infinite;
+       -webkit-border-top-right-radius: 8px;
+    -webkit-border-bottom-right-radius: 8px;
+           -moz-border-radius-topright: 8px;
+        -moz-border-radius-bottomright: 8px;
+               border-top-right-radius: 8px;
+            border-bottom-right-radius: 8px;
+        -webkit-border-top-left-radius: 20px;
+     -webkit-border-bottom-left-radius: 20px;
+            -moz-border-radius-topleft: 20px;
+         -moz-border-radius-bottomleft: 20px;
+                border-top-left-radius: 20px;
+             border-bottom-left-radius: 20px;
+    overflow: hidden;
+}
+
+.animate > span:after {
+    display: none;
+}
+
+@-webkit-keyframes move {
+    0% {
+       background-position: 0 0;
+    }
+    100% {
+       background-position: 50px 50px;
+    }
+}
+
+@-moz-keyframes move {
+    0% {
+       background-position: 0 0;
+    }
+    100% {
+       background-position: 50px 50px;
+    }
+}
+
+
+.orange > span {
+    background-color: #f1a165;
+    background-image: -moz-linear-gradient(top, #f1a165, #f36d0a);
+    background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #f1a165),color-stop(1, #f36d0a));
+    background-image: -webkit-linear-gradient(#f1a165, #f36d0a);
+}
+
+.red > span {
+    background-color: #f0a3a3;
+    background-image: -moz-linear-gradient(top, #f0a3a3, #f42323);
+    background-image: -webkit-gradient(linear,left top,left bottom,color-stop(0, #f0a3a3),color-stop(1, #f42323));
+    background-image: -webkit-linear-gradient(#f0a3a3, #f42323);
+}
+
+.nostripes > span > span, .nostripes > span:after {
+    -webkit-animation: none;
+    -moz-animation: none;
+    background-image: none;
+}
+""" ]
+        , H.div
+            [ HA.style
+                [ ( "display", "grid" )
+                , ( "grid-template-columns", "repeat(3,1fr)" )
+                , ( "font-size", "24px" )
+                ]
+            ]
+            [ model.exercises
+                |> List.head
+                |> Maybe.map (viewExercise model.currentExerciseState model.answerInput)
+                |> Maybe.withDefault viewWin
+            , model.lastAttempt
+                |> Maybe.map (viewLastAttempt model.lastAttemptState)
+                |> Maybe.withDefault (H.text "")
+            ]
+        , viewProgress model.allExercises model.exercises
+        ]
+
+
+viewProgress : Int -> List Exercise -> Html Msg
+viewProgress all currentExercises =
+    let
+        current : Int
+        current =
+            List.length currentExercises
+
+        done : Int
+        done =
+            all - current
+
+        donePercent : Float
+        donePercent =
+            toFloat done * 100 / toFloat all
+    in
+    H.div
+        [ HA.class "meter" ]
+        [ H.span
+            [ HA.style [ ( "width", toString donePercent ++ "%" ) ] ]
+            []
         ]
 
 
